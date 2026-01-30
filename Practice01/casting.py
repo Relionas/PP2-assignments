2e03ca2b88a9f5e21bf16372727786c2e440d257
Relionas <mandygulov08@mail.ru>
2026-01-30 11:00:09 +0500

    python basics

---
+++ Practice01/casting.py
@@ -0,0 +1,19 @@
+# Example 1: string to int
+s = "10"
+n = int(s)
+print(n)
+
+# Example 2: int to float
+f = float(5)
+print(f)
+
+# Example 3: float to int
+print(int(3.7))
+
+# Example 4: number to string
+age = 20
+print(str(age))
+
+# Example 5: boolean casting
+print(bool(0))
+print(bool(1))
