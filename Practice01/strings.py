4f76555dfc25aa1170e9e40953cb404867fa88ca
Relionas <mandygulov08@mail.ru>
2026-01-30 11:03:40 +0500

    python basics

--- Practice01/strings.py
+++ Practice01/strings.py
@@ -1,2 +1,16 @@
-#1
-print("Hello")
+# Example 1: string creation
+s1 = "Hello"
+print(s1)
+
+# Example 2: string concatenation
+s2 = "World"
+print(s1 + " " + s2)
+
+# Example 3: string length
+print(len(s1))
+
+# Example 4: string indexing
+print(s1[0])
+
+# Example 5: string methods
+print(s1.upper())
