12607cc831a81ec4fb274d57cf259cafcd48849f
Relionas <mandygulov08@mail.ru>
2026-01-30 11:04:49 +0500

    python basics

---
+++ Practice01/syntax_comments.py
@@ -0,0 +1,20 @@
+# Example 1: single-line comment
+y = 5  # this is a comment
+
+# Example 2: comment before code
+# printing a message
+print("Comments example")
+
+# Example 3: explaining logic
+z = 10  # value for calculation
+print(z * 2)
+
+# Example 4: commented out code
+# print("This will not run")
+
+# Example 5: multi-line comment style
+"""
+This is a multi-line comment
+used for documentation
+"""
+print("Done")
