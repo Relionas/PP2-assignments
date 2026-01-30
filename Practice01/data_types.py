48f0fb420acc5a0ee3b152741476a52c48770b92
Relionas <mandygulov08@mail.ru>
2026-01-30 11:02:08 +0500

    python_basics

---
+++ Practice01/data_types.py
@@ -0,0 +1,19 @@
+# Example 1: int
+num = 10
+print(type(num))
+
+# Example 2: float
+price = 9.99
+print(type(price))
+
+# Example 3: string
+text = "Python"
+print(type(text))
+
+# Example 4: boolean
+is_active = True
+print(type(is_active))
+
+# Example 5: list
+items = [1, 2, 3]
+print(type(items))
