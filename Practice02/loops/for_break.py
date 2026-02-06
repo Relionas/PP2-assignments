80c733add8221ecbb3a1b8662cf6fff6816872ec
Relionas <mandygulov08@mail.ru>
2026-02-06 12:07:55 +0500

    loops examples

---
+++ Practice02/loops/for_break.py
@@ -0,0 +1,31 @@
+
+# Example 1
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+# Example 2
+for letter in "Python":
+    if letter == "h":
+        break
+    print(letter)
+
+# Example 3
+numbers = [1, 2, 3, 4]
+for n in numbers:
+    if n == 3:
+        break
+    print(n)
+
+# Example 4
+for i in range(1, 6):
+    if i == 4:
+        break
+    print(i)
+
+# Example 5
+for x in range(10):
+    if x > 6:
+        break
+    print(x)
