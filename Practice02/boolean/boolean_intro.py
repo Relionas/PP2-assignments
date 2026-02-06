eecac09fb0ba8895adb0728b9910cc1bab1218e4
Relionas <mandygulov08@mail.ru>
2026-02-06 12:05:15 +0500

    boolean

---
+++ Practice02/boolean/boolean_intro.py
@@ -0,0 +1,16 @@
+# Example 1
+is_true = True
+print(is_true)
+
+# Example 2
+is_false = False
+print(is_false)
+
+# Example 3
+print(10 > 5)
+
+# Example 4
+print(3 == 4)
+
+# Example 5
+print(bool("Hello"))
