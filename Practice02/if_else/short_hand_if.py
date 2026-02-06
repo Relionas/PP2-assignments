683e423b699b7ad002bfaf360fcf06bdaf4db8fc
Relionas <mandygulov08@mail.ru>
2026-02-06 12:07:21 +0500

    if_else examples

---
+++ Practice02/if_else/short_hand_if.py
@@ -0,0 +1,20 @@
+# Example 1
+a = 5
+b = 10
+print("A") if a > b else print("B")
+
+# Example 2
+x = 7
+print("Even") if x % 2 == 0 else print("Odd")
+
+# Example 3
+age = 20
+print("Adult") if age >= 18 else print("Minor")
+
+# Example 4
+is_sunny = True
+print("Go out") if is_sunny else print("Stay home")
+
+# Example 5
+num = -1
+print("Positive") if num > 0 else print("Not positive")
