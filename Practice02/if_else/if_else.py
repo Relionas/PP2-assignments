88b4f81299b0391f440ae753d1a9b65ce19c276d
Relionas <mandygulov08@mail.ru>
2026-02-06 12:06:37 +0500

    if_else examples

---
+++ Practice02/if_else/if_else.py
@@ -0,0 +1,34 @@
+# Example 1
+x = 4
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Example 2
+age = 16
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+# Example 3
+password = "admin"
+if password == "admin":
+    print("Access granted")
+else:
+    print("Access denied")
+
+# Example 4
+num = -5
+if num > 0:
+    print("Positive")
+else:
+    print("Negative")
+
+# Example 5
+is_raining = False
+if is_raining:
+    print("Take umbrella")
+else:
+    print("No umbrella needed")
