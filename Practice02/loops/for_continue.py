48a0cebfd6930b97e7e592194cb4a3ec2809a6a0
Relionas <mandygulov08@mail.ru>
2026-02-06 12:08:28 +0500

    loops examples

---
+++ Practice02/loops/for_continue.py
@@ -0,0 +1,30 @@
+# Example 1
+for i in range(5):
+    if i == 2:
+        continue
+    print(i)
+
+# Example 2
+for letter in "Python":
+    if letter == "o":
+        continue
+    print(letter)
+
+# Example 3
+for i in range(1, 6):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+# Example 4
+numbers = [1, 2, 3, 4]
+for n in numbers:
+    if n == 3:
+        continue
+    print(n)
+
+# Example 5
+for i in range(10):
+    if i < 5:
+        continue
+    print(i)
