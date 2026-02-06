9b69023c055b221fef547d0f01d0582751c886d7
Relionas <mandygulov08@mail.ru>
2026-02-06 12:06:18 +0500

    if_else examples

---
+++ Practice02/if_else/if_elif_else.py
@@ -0,0 +1,44 @@
+# Example 1
+score = 85
+if score >= 90:
+    print("A")
+elif score >= 70:
+    print("B")
+else:
+    print("C")
+
+# Example 2
+num = 0
+if num > 0:
+    print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
+
+# Example 3
+age = 12
+if age < 13:
+    print("Child")
+elif age < 18:
+    print("Teen")
+else:
+    print("Adult")
+
+# Example 4
+day = 6
+if day == 1:
+    print("Monday")
+elif day == 6:
+    print("Saturday")
+else:
+    print("Other day")
+
+# Example 5
+temp = 15
+if temp > 25:
+    print("Hot")
+elif temp > 10:
+    print("Warm")
+else:
+    print("Cold")
