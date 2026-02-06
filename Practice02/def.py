8d83957736acadcf8215c24c1b6da4f67662f3e7
Relionas <mandygulov08@mail.ru>
2026-02-06 15:04:39 +0500

    def

---
+++ Practice02/def.py
@@ -0,0 +1,16 @@
+name = [
+    {'name': 'Dias', 'id': '001'},
+    {'name': 'Niyaz', 'id': '002'},
+    {'name': 'Almas', 'id': '003'}
+]
+
+gpa = [
+    {'id': '001', 'gpa': '3.85'},
+    {'id': '002', 'gpa': '3.54'},
+    {'id': '003', 'gpa': '3.36'}
+]
+
+for i in name:
+    for s in gpa:
+        if i['id'] == s['id']:
+            print(i['name'], "-", s['gpa'])
