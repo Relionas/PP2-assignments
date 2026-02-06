4592ee085d428f027bfe61839472fa4cb1ecc858
Relionas <mandygulov08@mail.ru>
2026-02-06 12:06:54 +0500

    if_else examples

---
+++ Practice02/if_else/if_statement.py
@@ -0,0 +1,24 @@
+# Example 1
+x = 10
+if x > 5:
+    print("x is greater than 5")
+
+# Example 2
+age = 18
+if age >= 18:
+    print("Adult")
+
+# Example 3
+num = 3
+if num > 0:
+    print("Positive")
+
+# Example 4
+temperature = 30
+if temperature > 25:
+    print("Hot")
+
+# Example 5
+score = 90
+if score == 90:
+    print("Perfect")
